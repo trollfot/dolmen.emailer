cc0b10436cb89e990dede347095376616bb3579b
Souheil CHELFOUH <trollfot@gmail.com>
2010-11-30 19:11:51 +0100

    Added base utility

---
+++ src/dolmen/emailer/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+from dolmen.emailer.utility import get_sender, MailSender, IMailSender
---
+++ src/dolmen/emailer/utility.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+import grokcore.component as grok
+import email.MIMEText, email.Header
+
+from zope.interface import Interface
+from zope.component import getUtility
+from zope.sendmail.interfaces import IMailDelivery
+
+
+class IMailSender(Interface):
+    """A mail sender definition.
+    """
+    def send(recipients, subject, body, sender=None):
+        """Send a mail to the given recipients.
+        """
+
+
+class MailSender(grok.GlobalUtility):
+    grok.baseclass()
+    grok.implements(IMailSender)
+
+    mailer_name = u"dolmen.mailer"
+    admin_email = u"service@mysite.com"
+
+    def send(self, recipients, subject, body, sender=None):
+        if sender is None:
+            sender = self.admin_email
+        
+        mailer = getUtility(IMailDelivery, self.mailer_name)
+        msg = email.MIMEText.MIMEText(body.encode('UTF-8'), 'plain', 'UTF-8')
+        msg["Subject"] = email.Header.Header(subject, 'UTF-8')
+        msg["From"] = sender
+
+        for recipient in recipients:
+            msg["To"] = recipient
+            mailer.send(sender, [recipient], msg.as_string())
+
+
+def get_sender(name=u''):
+    return getUtility(IMailSender, name=name)
